b28c37ab751681653541d0a8fcb8ac8227cfbe32
KIFLDY <66375338+KIFLDY@users.noreply.github.com>
2024-01-11 12:30:27 +0400

    Add files via upload

---
+++ task1/task1/task1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int next_i(int i, int n, int m) {
+    return 1 + (i + m - 2) % n;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    int i = 1;
+    while (true) {
+        cout << i << " ";
+        i = next_i(i, n, m);
+        if (i == 1) {
+            break;
+        }
+    }
+    cout << endl;
+    return 0;
+}
